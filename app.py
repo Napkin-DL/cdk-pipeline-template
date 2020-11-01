87e643e645fd56120e4eea5f5b978530f9a084ef
root <root@DESKTOP-USO0D8E.localdomain>
2020-11-02 01:20:33 +0900

    Template project to bootstrap cdk-pipeline

---
+++ app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import configparser
+from aws_cdk import core
+
+from pipeline_stack.cdk_pipeline_stack import CdkPipelineStack as pipeline
+
+config = configparser.ConfigParser()
+config.read('app.cfg')
+app = core.App()
+
+pipeline(app, config['app']['stack_name'], 
+    env={
+        'account': config['app']['account'],
+        'region': config['app']['region']
+    },
+    pipeline_config={
+        'pipeline_name': config['pipeline']['pipeline_name'],
+        'github_oauth_token': config['pipeline']['github_oauth_token'],
+        'github_owner': config['pipeline']['github_owner'],
+        'github_repo': config['pipeline']['github_repo'],
+        'github_branch': config['pipeline']['github_branch']
+    }
+    )
+
+app.synth()
---
+++ pipeline_stack/cdk_pipeline_stack.py
@@ -0,0 +1,33 @@
+from aws_cdk import core
+from aws_cdk import aws_codepipeline as codepipeline
+from aws_cdk import aws_codepipeline_actions as cpactions
+from aws_cdk import pipelines
+
+
+class CdkPipelineStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, pipeline_config: dict, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        source_artifact = codepipeline.Artifact()
+        cloud_assembly_artifact = codepipeline.Artifact()
+
+        pipelines.CdkPipeline(self, 'CdkPipeline', 
+            cloud_assembly_artifact=cloud_assembly_artifact,
+            pipeline_name=pipeline_config['pipeline_name'],            
+            source_action=cpactions.GitHubSourceAction(
+                # https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-codepipeline-actions.GitHubSourceActionProps.html
+                action_name='GithubAction',
+                output=source_artifact,
+                oauth_token=core.SecretValue.secrets_manager(pipeline_config['github_oauth_token']),
+                # https://docs.aws.amazon.com/ko_kr/codepipeline/latest/userguide/welcome.html
+                owner=pipeline_config['github_owner'],
+                repo=pipeline_config['github_repo'],
+                branch=pipeline_config['github_branch'],
+                trigger=cpactions.GitHubTrigger.POLL),
+            synth_action=pipelines.SimpleSynthAction(
+                source_artifact=source_artifact,
+                cloud_assembly_artifact=cloud_assembly_artifact,
+                install_command='npm install -g aws-cdk && pip install -r requirements.txt',
+                synth_command='cdk synth'
+            )
+        )
